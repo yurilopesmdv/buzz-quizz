d50e04206a18872c3bcf9497713358577771baad
yurilopesm <yurilopesm@hotmail.com>
2023-01-25 17:00:39 -0300

    Template - Renderizando a tela1

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function renderizarTela1() {
+   document.body.innerHTML = ` <div class="tela1">
+   <div class="header">
+       <h1>BuzzQuizz</h1>
+   </div>
+   <div class="conteudo-tela1">
+       <div class="div-seus-quizzes">
+           <p>Você não criou nenhum</br> quizz ainda :(</p>
+           <button class="botao-criar-quizz">Criar Quizz</button>
+       </div>
+       <h2>Todos os Quizzes</h2>
+       <div class="div-todos-quizzes">
+           <div class="quizz"></div>
+           <div class="quizz"></div>
+           <div class="quizz"></div>
+           <div class="quizz"></div>
+           <div class="quizz"></div>
+           <div class="quizz"></div>
+       </div>
+   </div>
+</div>`
+}
+renderizarTela1();
