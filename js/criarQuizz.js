7b783133d576fb43f2c53b5b9a7cf4a98ad9bad0
Wesley <wesleymichaelpereira@hotmail.com>
2023-01-26 16:35:28 -0300

    Renderização dinâmica da tela 3.2 - Perguntas do Quizz

--- js/criarQuizz.js
+++ js/criarQuizz.js
@@ -1,18 +1,173 @@
 /**** Variáveis globais ****/
 const infoBasicas = {
-    tituloQuizz: '',
-    urlImage: '',
+    tituloQuizz: 'Michael',
+    urlImage: 'https://urlimagem',
     quantPerguntas: 0,
     quantNiveis: 0,
 }
 
-let quizz = [];
+let quizz = {
+    title: '',
+    image:'',
+    questions:[],
+    levels: [],
+}
+
+let question = {
+    title: '',
+    color: '',
+    answers: [],
+}
+
+const answer = {
+    text: '',
+    image: '',
+    isCorrectAnswer: false,
+}
+
+let questions = [];
+
+let answers = [];
 
 
+const preencheAnswers = (objetoAns) => answers.push(objetoAns);
+
+
+
+function mostrarPergunta(iconSelecionado){
+    //Quando clicar no icon só vai vir a caixinha do icone, dai pegamos o box pai para adicionar a classe 'selecionado'
+    const perguntaSelecionada = iconSelecionado.parentNode.parentNode;
+    
+    const boxAtualmenteSelecionada = document.querySelector('.selecionado');
+
+    //Antes de adicionar a classe no icone escolhido, vamos remover a já existente.
+    boxAtualmenteSelecionada.classList.add('hidden');
+    boxAtualmenteSelecionada.classList.remove('selecionado');
+
+    perguntaSelecionada.classList.remove('hidden');
+    perguntaSelecionada.classList.add('selecionado');
+}
+
+
+//////////////////////////////  Função para renderizar a página de criação das perguntas do quizz ///////////////////////////////////
 
 function submitInfoBasicasQuizz(){
-    //renderizar próxima página
+    //renderizar tela 3.2
+    const tela = document.querySelector('.criarQuizz');
+
+    tela.innerHTML = `
+        <div class="criarQuizz-header">
+            <h2>Crie suas perguntas</h2>
+        </div>`;
+    
+    for( let i = 1; i <= infoBasicas.quantPerguntas; i++){
+        if(i === 1){
+            tela.innerHTML += `
+            <div class="selecionado">
+                <div class="criarQuizz-main">
+                    <div class="criarQuizz-form">
+                        <h2>Pergunta ${i}</h2>
+                        <div><input type="text" placeholder="Texto da pergunta"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Cor de fundo da pergunta"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <h2>Resposta correta</h2>
+                        <div><input type="text" placeholder="Resposta correta"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da resposta correta"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <h2>Respostas incorretas</h2>
+                        <div><input type="text" placeholder="Resposta incorreta 1"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 1"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Resposta incorreta 2"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 2"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Resposta incorreta 3"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 3"></div>
+                    </div>   
+                </div>
+                <div class="box-pergunta-resumida">
+                    <h2>Pergunta ${i}</h2>
+                    <ion-icon onclick="mostrarPergunta(this)" name="create-outline"></ion-icon>
+                </div>
+            </div>`; 
+        } else {
+            tela.innerHTML += `
+            <div class="hidden">
+                <div class="criarQuizz-main">
+                    <div class="criarQuizz-form">
+                        <h2>Pergunta ${i}</h2>
+                        <div><input type="text" placeholder="Texto da pergunta"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Cor de fundo da pergunta"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <h2>Resposta correta</h2>
+                        <div><input type="text" placeholder="Resposta correta"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da resposta correta"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <h2>Respostas incorretas</h2>
+                        <div><input type="text" placeholder="Resposta incorreta 1"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 1"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Resposta incorreta 2"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 2"></div>
+                    </div>
+                    <hr>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="Resposta incorreta 3"></div>
+                    </div>
+                    <div class="criarQuizz-form">
+                        <div><input type="text" placeholder="URL da imagem 3"></div>
+                    </div>   
+                </div>
+                <div class="box-pergunta-resumida">
+                    <h2>Pergunta ${i}</h2>
+                    <ion-icon onclick="mostrarPergunta(this)" name="create-outline"></ion-icon>
+                </div>
+            </div>`;
+        }
+        tela.innerHTML += `<hr>`;
+    }
+    tela.innerHTML += `
+        <div>
+            <button class="btn-red" type="submit" name="button" onclick="submitInfoBasicasQuizz()" disabled>
+                Prosseguir para criar níveis
+            </button>
+            <hr><hr><hr><hr><hr><hr><hr>
+        </div>
+    `;
 }
+
 
 
 ///////////////////////////////  INFORMAÇÕES BÁSICAS DO QUIZZ  ///////////////////////////////////////////
