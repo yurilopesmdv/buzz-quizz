d77272f30354738b031a42213374a41b8f45cc7b
Wesley <wesleymichaelpereira@hotmail.com>
2023-01-27 01:41:08 -0300

    Validação das entradas da tela 3.2

--- js/criarQuizz.js
+++ js/criarQuizz.js
@@ -1,55 +1,127 @@
 /**** Variáveis globais ****/
-const infoBasicas = {
-    tituloQuizz: 'Michael',
-    urlImage: 'https://urlimagem',
-    quantPerguntas: 0,
-    quantNiveis: 0,
-}
+let quantPerguntas = 1;
+let quantNiveis = 2;
 
-let quizz = {
+const quizz = {
     title: '',
     image:'',
     questions:[],
     levels: [],
 }
 
-let question = {
-    title: '',
-    color: '',
-    answers: [],
-}
 
-const answer = {
-    text: '',
-    image: '',
-    isCorrectAnswer: false,
-}
-
-let questions = [];
-
-let answers = [];
+//Função que preenche o atributo questions do quizz
+const preencheQuestions = (objetoQuestion) => quizz.questions.push(objetoQuestion);
 
 
-const preencheAnswers = (objetoAns) => answers.push(objetoAns);
-
-
-
-function mostrarPergunta(iconSelecionado){
-    //Quando clicar no icon só vai vir a caixinha do icone, dai pegamos o box pai para adicionar a classe 'selecionado'
-    const perguntaSelecionada = iconSelecionado.parentNode.parentNode;
-    
-    const boxAtualmenteSelecionada = document.querySelector('.selecionado');
-
-    //Antes de adicionar a classe no icone escolhido, vamos remover a já existente.
-    boxAtualmenteSelecionada.classList.add('hidden');
-    boxAtualmenteSelecionada.classList.remove('selecionado');
-
-    perguntaSelecionada.classList.remove('hidden');
-    perguntaSelecionada.classList.add('selecionado');
+function renderizaNiveisQuizz(){
+    //Função para renderizar os níveis do quizz
 }
 
 
-//////////////////////////////  Função para renderizar a página de criação das perguntas do quizz ///////////////////////////////////
+//Função para armazenar as perguntas em caso de sucesso nas validações
+function preencheQuizzPerguntas(entradas){
+    //Parâmetro 'entradas' armazena todos os valores de input de cada pergunta individualmente
+    const question = {
+        title: entradas[0].value,
+        color: entradas[1].value,
+        answers: [],
+    }
+    //primeira resposta inserida é a resposta correta
+    for (let i = 2; i < entradas.length; i+=2){
+        const answer = {
+            text: entradas[i].value,
+            image: entradas[i+1].value,
+            isCorrectAnswer: false,
+        }
+        //Primeira questão é a correta
+        if( i === 2 ){
+            answer.isCorrectAnswer = true;
+        }
+        //Garantir que só será copiado se o par pergunta + URL for digitado (para das respostas que não são obrigatórias)
+        if( entradas[i].value !== '' && entradas[i+1].value !== ''){
+            question.answers.push(answer);
+        }
+    }
+    preencheQuestions(question);
+    renderizaNiveisQuizz();
+}
+
+
+//Função auxiliar. Verificar se todas as validações foram verificadas
+function submitPerguntasQuizz(){
+    if( checkInput() && checkInputValidados() ){
+        //NodeList com todas as perguntas preenchidas
+        const todasPerguntas = document.querySelectorAll('.criarQuizz-pergunta')
+        
+        for(let i = 0; i < todasPerguntas.length; i++){
+            preencheQuizzPerguntas( todasPerguntas[i].querySelectorAll('input') );
+        }
+    }
+}
+
+
+//Função que verifica se todos os inputs foram preenchidos corretamente
+function checkInputValidados(){
+    if(document.querySelector('.invalido') === null){
+        return true;
+    }
+    return false;
+}
+
+
+//Função que verifica se os inputs obrigatórios foram preenchidos antes de armazenar informações
+function checkInput(){
+    const inputObrigatorios = document.querySelectorAll('[required]');
+
+    for(let i in inputObrigatorios){
+        if(inputObrigatorios[i].value === ''){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+/////////////////////////////////////            Validação das perguntas do quizz          ///////////////////////////////////
+
+//Função para validar a quantidade de caracteres na pergunta
+function validarPergunta(pergunta){
+    const divPai = pergunta.parentNode;
+    const quantCaracterMinPergunta = 20;
+
+    if(pergunta.value.length < quantCaracterMinPergunta){
+        divPai.classList.add('invalido');
+    } else {
+        divPai.classList.remove('invalido');
+    }
+}
+
+//Função para validar cor hexadecimal
+function validarCor(cor){
+    const divPai = cor.parentNode;
+
+    let regex = new RegExp(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/);
+
+    if( regex.test(cor.value) === true){
+        divPai.classList.remove('invalido');
+    } else {
+        divPai.classList.add('invalido');
+    }   
+}
+
+function validarURL(url){
+    const divPai = url.parentNode;
+
+    if (isUrl(url.value)){
+        divPai.classList.remove('invalido');
+    } else {
+        divPai.classList.add('invalido');
+    }
+}
+
+
+////////////////////////////// Função para renderizar a página de criação das perguntas do quizz ///////////////////////////////////
 
 function submitInfoBasicasQuizz(){
     //renderizar tela 3.2
@@ -60,33 +132,51 @@ function submitInfoBasicasQuizz(){
             <h2>Crie suas perguntas</h2>
         </div>`;
     
-    for( let i = 1; i <= infoBasicas.quantPerguntas; i++){
+    for( let i = 1; i <= quantPerguntas; i++){
         if(i === 1){
             tela.innerHTML += `
             <div class="selecionado">
-                <div class="criarQuizz-main">
+                <div class="criarQuizz-pergunta">
                     <div class="criarQuizz-form">
                         <h2>Pergunta ${i}</h2>
-                        <div><input type="text" placeholder="Texto da pergunta"></div>
+                        <div>
+                            <input type="text" placeholder="Texto da pergunta" onchange="validarPergunta(this)" required>
+                            <span>Deve ter no mínimo 20 caracteres</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="Cor de fundo da pergunta"></div>
+                        <div>
+                            <input type="text" placeholder="Cor de fundo da pergunta. Ex: #FAFAFA" onchange="validarCor(this)" required>
+                            <span>Cor deve ser no formato hexadecimal</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
                         <h2>Resposta correta</h2>
-                        <div><input type="text" placeholder="Resposta correta"></div>
+                        <div>
+                            <input type="text" placeholder="Resposta correta" required>
+                            <span>Deve ser inserido uma resposta correta</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="URL da resposta correta"></div>
+                        <div>
+                            <input type="url" placeholder="URL da resposta correta" onchange="validarURL(this)" required>
+                            <span>URL inválida</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
                         <h2>Respostas incorretas</h2>
-                        <div><input type="text" placeholder="Resposta incorreta 1"></div>
+                        <div>
+                            <input type="text" placeholder="Resposta incorreta 1" required>
+                            <span>Deve ser inserido pelo menos uma resposta incorreta</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="URL da imagem 1"></div>
+                        <div>
+                            <input type="url" placeholder="URL da imagem 1" onchange="validarURL(this)" required>
+                            <span>URL inválida</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
@@ -111,29 +201,47 @@ function submitInfoBasicasQuizz(){
         } else {
             tela.innerHTML += `
             <div class="hidden">
-                <div class="criarQuizz-main">
+                <div class="criarQuizz-pergunta">
                     <div class="criarQuizz-form">
                         <h2>Pergunta ${i}</h2>
-                        <div><input type="text" placeholder="Texto da pergunta"></div>
+                        <div>
+                            <input type="text" placeholder="Texto da pergunta" onchange="validarPergunta(this)" required>
+                            <span>Deve ter no mínimo 20 caracteres</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="Cor de fundo da pergunta"></div>
+                        <div>
+                            <input type="text" placeholder="Cor de fundo da pergunta. Ex: #FAFAFA" onchange="validarCor(this)" required>
+                            <span>Cor deve ser no formato hexadecimal</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
                         <h2>Resposta correta</h2>
-                        <div><input type="text" placeholder="Resposta correta"></div>
+                        <div>
+                            <input type="text" placeholder="Resposta correta" required>
+                            <span>Deve ser inserido uma resposta correta</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="URL da resposta correta"></div>
+                        <div>
+                            <input type="url" placeholder="URL da resposta correta" required>
+                            <span>URL inválida</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
                         <h2>Respostas incorretas</h2>
-                        <div><input type="text" placeholder="Resposta incorreta 1"></div>
+                        <div>
+                            <input type="text" placeholder="Resposta incorreta 1" required>
+                            <span>Deve ser inserido pelo menos uma resposta incorreta</span>
+                        </div>
                     </div>
                     <div class="criarQuizz-form">
-                        <div><input type="text" placeholder="URL da imagem 1"></div>
+                        <div>
+                            <input type="url" placeholder="URL da imagem 1" onchange="validarURL(this)" required>
+                            <span>URL inválida</span>
+                        </div>
                     </div>
                     <hr>
                     <div class="criarQuizz-form">
@@ -160,17 +268,31 @@ function submitInfoBasicasQuizz(){
     }
     tela.innerHTML += `
         <div>
-            <button class="btn-red" type="submit" name="button" onclick="submitInfoBasicasQuizz()" disabled>
+            <button class="btn-red" type="submit" name="button" onclick="submitPerguntasQuizz()">
                 Prosseguir para criar níveis
             </button>
-            <hr><hr><hr><hr><hr><hr><hr>
+            <hr><hr>
         </div>
     `;
 }
 
+//Função para navegar na página de criação das perguntas do quizz
+function mostrarPergunta(iconSelecionado){
+    //Quando clicar no icon só vai vir a caixinha do icone, dai pegamos o box pai para adicionar a classe 'selecionado'
+    const perguntaSelecionada = iconSelecionado.parentNode.parentNode;
+    
+    const boxAtualmenteSelecionada = document.querySelector('.selecionado');
+
+    //Antes de adicionar a classe no icone escolhido, vamos remover a já existente.
+    boxAtualmenteSelecionada.classList.add('hidden');
+    boxAtualmenteSelecionada.classList.remove('selecionado');
+
+    perguntaSelecionada.classList.remove('hidden');
+    perguntaSelecionada.classList.add('selecionado');
+}
 
 
-///////////////////////////////  INFORMAÇÕES BÁSICAS DO QUIZZ  ///////////////////////////////////////////
+////////////////////////////////////    INFORMAÇÕES BÁSICAS DO QUIZZ      ///////////////////////////////////////////
 
 //Função para habilitar o botão de submit das informações básicas do quizz
 function verificarTodasInfoBasicasQuizz(){
@@ -189,12 +311,12 @@ function verificarTodasInfoBasicasQuizz(){
 //Função para validar a quantidade de niveis no quizz
 //OBS: classe 'valido' usada apenas para controle das informações válidas que forem inseridas
 function validarQuantNiveisQuizz({target}){
-    infoBasicas.quantNiveis = target.value;
+    quantNiveis = target.value;
     const quantNiveisMin = 2;
 
     const quantNiveisAlerta = document.querySelector('.criarQuizz-form-quantNiveis');
 
-    if( infoBasicas.quantNiveis < quantNiveisMin || isNaN(infoBasicas.quantNiveis) ){
+    if( quantNiveis < quantNiveisMin || isNaN(quantNiveis) ){
         quantNiveisAlerta.classList.remove('valido');
         quantNiveisAlerta.classList.add('invalido');
 
@@ -209,12 +331,12 @@ function validarQuantNiveisQuizz({target}){
 
 //Função para validar a quantidade de perguntas no quizz
 function validarQuantPerguntasQuizz({target}){
-    infoBasicas.quantPerguntas = target.value;
+    quantPerguntas = target.value;
     const quantPerguntasMin = 3;
 
     const quantPerguntasAlerta = document.querySelector('.criarQuizz-form-quantPerguntas');
 
-    if( infoBasicas.quantPerguntas < quantPerguntasMin || isNaN(infoBasicas.quantPerguntas)){
+    if( quantPerguntas < quantPerguntasMin || isNaN(quantPerguntas)){
         quantPerguntasAlerta.classList.remove('valido');
         quantPerguntasAlerta.classList.add('invalido');
     } else {
@@ -226,19 +348,27 @@ function validarQuantPerguntasQuizz({target}){
 
 
 //Função para validar uma url
+function isUrl(urlString){
+    //Recebe uma string e se conseguir instanciar vamos considerar válido
+    try {
+        let url = new URL(urlString);
+        return true;
+
+    } catch (erro) {
+        return false;
+    }
+}
+
 function validarUrlQuizz({target}){
     //url no formato de string
-    infoBasicas.urlImage = target.value;
+    quizz.image = target.value;
 
     const urlAlerta = document.querySelector('.criarQuizz-form-url');
     
-    try {
-        //Converter para o formato de objeto URL
-        let url = new URL(infoBasicas.urlImage);
+    if (isUrl(quizz.image)){
         urlAlerta.classList.remove('invalido');
         urlAlerta.classList.add('valido');
-
-    } catch (erro) {
+    } else {
         urlAlerta.classList.remove('valido');
         urlAlerta.classList.add('invalido');
     }
@@ -248,12 +378,12 @@ function validarUrlQuizz({target}){
 
 //Função para validar o título do quizz
 function validarTituloQuizz({target}){
-    infoBasicas.tituloQuizz = target.value;
+    quizz.title = target.value;
     const caracterMax = 65;
     const caracterMin = 20;
     const tituloAlerta = document.querySelector('.criarQuizz-form-titulo');
 
-    if( infoBasicas.tituloQuizz.length < caracterMin || infoBasicas.tituloQuizz.length > caracterMax ){
+    if( quizz.title.length < caracterMin || quizz.title.length > caracterMax ){
         tituloAlerta.classList.remove('valido');
         tituloAlerta.classList.add('invalido');
 
