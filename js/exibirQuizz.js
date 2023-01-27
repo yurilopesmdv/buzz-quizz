047100e33d992a62803eeefe477cd27e73379692
yurilopesm <yurilopesm@hotmail.com>
2023-01-27 19:13:32 -0300

    Tela1 renderizando com todos os quizzes

---
+++ js/exibirQuizz.js
@@ -0,0 +1,33 @@
+/*<div class="conteudo">
+            <div class="container">
+                '<div class="topo">
+                    BuzzQuizz
+                </div>
+                <div class="cabecalhoQuizz">
+                    <div class="imagemQuizz">
+                        <img src="./img/flor.jpg" width="1440px" height="227px">
+                        
+                    </div>
+                    <div class="escurecer"></div>
+                    <div class="nomeQuizz">
+                        O quão Potterhead é você?
+                    </div>
+                </div>
+                <div class="corpoQuizz">
+                    <div class="pergunta">
+                        <div class="caixaPergunta">
+                            Em qual animal Olho-Tonto transfigurou Malfoy?
+                        </div>
+                        <div class="alternativa">
+                            <button>
+                                <div class="imagemAlternativa">
+                                    <img src="./img/flor.jpg" width="320px" height="175px">
+                                    Gato
+                                </div>
+                            </button>
+                        </div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        */
--- js/script.js
+++ js/script.js
@@ -1,4 +1,82 @@
+const conteudo = document.querySelector('.conteudo');
+let quizzesUsuario = [];
+let todosQuizzes = [];
 
+function processarQuizzes() {
+    conteudo.scrollTo(0, 0);
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promise.then(renderizarTela1);
+    promise.catch(errorGetQuizz);
+}
+function errorGetQuizz() {
+    alert('Erro ao receber os quizzes, Por favor, recarregue a página.');
+}
+function renderizarTela1(resposta) {
+    const quizzesServidor = resposta.data;
+    const quizzSeparado = separaQuizzUsuario(quizzesServidor);
+    quizzesUsuario = quizzSeparado.usuario;
+    todosQuizzes = quizzSeparado.todos;
+    renderizarQuizzes();
+}
+function separaQuizzUsuario(listaQuizzes) {
+    const quizzesSeparados = {
+        usuario: [],
+        todos: []
+    };
+    for(let i = 0; i < listaQuizzes.length; i++) {
+        const quizz = listaQuizzes[i];
+        if(quizzEhDoUsuario(quizz)) {
+            quizzesSeparados.usuario.push(quizz);
+            
+        } else {
+            quizzesSeparados.todos.push(quizz);
+        }
+    }
+    return quizzesSeparados;
+}
+function quizzEhDoUsuario(quizz) {
+    const quizzesDoUsuario = getQuizzesLocal();
+    for(let i = 0; i < quizzesDoUsuario.length; i++) {
+        if(quizzesDoUsuario[i].id === quizz.id) {
+            return true;
+        }
+    }
+   
+    return false;
+}
+function getQuizzesLocal() {
+    let dadosLocais = localStorage.getItem('quizz');
+    if(dadosLocais !== null) {
+       const dadosLocaisSalvos = JSON.parse(dadosLocais);
+        return dadosLocaisSalvos;
+    } else {
+        return [];
+    }
+}
+function renderizarQuizzes() {
+    let divUsuario = '';
+    if(quizzesUsuario.length === 0) {
+        divUsuario = gerarDivUsuarioVazio();
+    } else {
+        divUsuario = gerarDivComQuizzUsuario();
+    }
+    let divTodos = '';
+    todosQuizzes.forEach(function (quizz) {
+        divTodos = divTodos + gerarQuizzes(quizz);
+    });
+    conteudo.innerHTML = `
+    <div class="header">
+        <h1>BuzzQuizz</h1>
+    </div>
+    <div class="conteudo-tela1">
+        ${divUsuario}
+        <h2>Todos os Quizzes</h2>
+        <div class="div-todos-quizzes">
+            ${divTodos}
+        </div>
+    </div>
+    </div>`
+}
 
 function gerarDivUsuarioVazio() {
     return `
