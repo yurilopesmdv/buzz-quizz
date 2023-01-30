c454968127a7b6f54802b7e87335907cf275ee4b
yurilopesm <yurilopesm@hotmail.com>
2023-01-30 16:57:44 -0300

    versao restaurada correcao da tela1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,11 @@ let quizzesUsuario = [];
 let todosQuizzes = [];
 let resposta = 0;
 let data;
+let acertos = 0;
+let perguntasRespondidas = 0;
+let contadorAlternativas = 1
+let quantidadePerguntas = 0;
+let perguntaRespondida;
 
 function processarQuizzes() {
     conteudo.scrollTo(0, 0);
@@ -39,17 +44,16 @@ function separaQuizzUsuario(listaQuizzes) {
 function quizzEhDoUsuario(quizz) {
     const quizzesDoUsuario = getQuizzesLocal();
     for(let i = 0; i < quizzesDoUsuario.length; i++) {
-        if(quizzesDoUsuario[i].id === quizz.id) {
+        if(quizzesDoUsuario[i].id == quizz.id) {
             return true;
         }
     }
-   
     return false;
 }
 function getQuizzesLocal() {
-    let dadosLocais = localStorage.getItem('quizz');
+    let dadosLocais = localStorage.getItem('id');
     if(dadosLocais !== null) {
-       const dadosLocaisSalvos = JSON.parse(dadosLocais);
+        const dadosLocaisSalvos = JSON.parse(dadosLocais);
         return dadosLocaisSalvos;
     } else {
         return [];
@@ -65,7 +69,7 @@ function renderizarQuizzes() {
     let divTodos = '';
     todosQuizzes.forEach(function (quizz) {
         divTodos = divTodos + gerarQuizzes(quizz);
-    });
+    })
     conteudo.innerHTML = `
     <div class="header">
         <h1>BuzzQuizz</h1>
@@ -90,7 +94,7 @@ function gerarDivUsuarioVazio() {
 function gerarDivComQuizzUsuario() {
     let listaQuizzUsuario = '';
     quizzesUsuario.forEach(function (quizz) {
-        listaQuizzUsuario += gerarQuizzes();
+        listaQuizzUsuario += gerarQuizzes(quizz);
     });
     return `
             <div class="div-seus-quizzes">
@@ -107,15 +111,12 @@ function gerarQuizzes(quizz) {
     return `
     <div class="quizz" onclick="exibirQuizz(${quizz.id})">
         <img src="${quizz.image}">
-        <div class="degrade">a</div>
+        <div class="degrade"></div>
         <div class="titulo-quizz-tela1">${quizz.title}</div>
     </div>`
 }
-function selecionar (){
-    console.log('0')
-}
 function mostrarQuizz(definir){
-    let data = definir.data
+    data = definir.data
     const exibir = document.querySelector('.conteudo');
     exibir.innerHTML = `
     <div class="container">
@@ -133,45 +134,105 @@ function mostrarQuizz(definir){
     </div>
     <div class="corpoQuizz">
         
-      </div>
-  </div>`;
+      </div>`;
 
   gerarPerguntas()
   function gerarRespostas(){
     for (let m = 0; m < data.questions[i].answers.length; m++){
         let n = Math.floor(Math.random() * (m));
         [data.questions[i].answers[m], data.questions[i].answers[n]] = [data.questions[i].answers[n], data.questions[i].answers[m]];
-        console.log(data.questions[i].answers);
+        
       }
     for(j=0; j < data.questions[i].answers.length; j++){
-        const respostas = document.querySelector('.corpoQuizz');
+        
+        const respostas = document.querySelector(`.pergunta${i+1}`);
+        
         respostas.innerHTML = respostas.innerHTML + `
-            <button onclick= "(selecionar(this))" class="alternativa alternativa1">
-                 <div class="imagemAlternativa">
-                    <img src="${data.questions[i].answers[j].image}" width="320px" height="175px">
-                    ${data.questions[i].answers[j].text}
-                  </div>
-             </button>
+            
+                <button onclick= "(selecionar(this))" class="alternativa alternativa${j+1}">
+                    <div class="imagemAlternativa">
+                        <img src="${data.questions[i].answers[j].image}" width="320px" height="175px">
+                        ${data.questions[i].answers[j].text}
+                    </div>
+                </button>    
         `
-    }
+        
+        if(contadorAlternativas == data.questions[i].answers.length){
+            
+            respostas.innerHTML = respostas.innerHTML + `
+            </div>`
+            contadorAlternativas = 1;
+        }else{
+            contadorAlternativas++;  
+        }
+        
+    }  
   }
   function gerarPerguntas(){
     for(i=0; i < data.questions.length; i++ ){
         const perguntas = document.querySelector('.corpoQuizz');
         perguntas.innerHTML = perguntas.innerHTML + `
-        <div class="pergunta">
+        <div class="pergunta pergunta${i+1}">
             <div class="caixaPergunta">
                 ${data.questions[i].title}
-            </div>`
+            </div>
+        `
+        quantidadePerguntas++;
             gerarRespostas()
+
     }
   }
-  console.log(data)
+  
 }
+function selecionar(selecionado){
+    perguntasRespondidas++;
+    selecionado.classList.add('selecionado');
+    const parent = selecionado.parentNode;
+    parent.classList.add('desabilitar')
+    let todasAlternativas = parent.querySelectorAll('.alternativa')
+    todasAlternativas.forEach(res =>{
+     for(i=0; i < todasAlternativas.length; i++){
+        if(todasAlternativas[i].classList.contains('selecionado')){
 
+        }else{
+            todasAlternativas[i].classList.add('esbranquicado')
+        }  
+     }
+     for(y=0; y < quantidadePerguntas; y++){
+     for(z = 0; z < data.questions[y].answers.length; z++ ){
+        if(data.questions[y].answers[z].isCorrectAnswer === true){
+
+        document.querySelector(`.pergunta${y+1} .alternativa${z+1}`).classList.add('correta')
+        }else{
+        document.querySelector(`.pergunta${y+1} .alternativa${z+1}`).classList.add('incorreta')   
+        }
+
+        }
+     }
+     for(i=0; i < todasAlternativas.length; i++){
+        if(todasAlternativas[i].classList.contains('correta')){
+            todasAlternativas[i].classList.add('certa')
+        }else{
+            todasAlternativas[i].classList.add('errada')
+        }  
+        if(perguntasRespondidas === quantidadePerguntas){
+            for(i=0; i < todasAlternativas.length; i++){
+                if(todasAlternativas[i].classList.contains('correta') && todasAlternativas[i].classList.contains('selecionado') ){
+                    acertos++;
+                    console.log(acertos)
+                    
+                }  
+            }
+        }
+     }
+     
+    })
+    
+}
 processarQuizzes();
 function exibirQuizz(id){
     console.log(id);
     let quizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${id}`);
     quizz.then(mostrarQuizz);
+
 }
